e42aefc5cf1c9baadcb57126cafdb4790724cc6f
Shawn Feng <shawnsfeng@gmail.com>
2017-05-24 16:17:26 -0700

    implement isDiagPathClear function in server isLegalMove.js

--- server/chess/isLegalMove.js
+++ server/chess/isLegalMove.js
@@ -1,7 +1,7 @@
 // origin/dest are arrays that represent coordinates [M, N]
 
 const isVertPathClear = (game, origin, dest, limit = 7) => {
-  console.log('Origin: ', origin, game.board[origin[0]][origin[1]]);
+  console.log('Origin: ', origin, board[origin[0]][origin[1]]);
   console.log('Dest: ', dest, game.board[dest[0]][dest[1]]);
 
   for (let i = origin[0] + 1; i < dest[0]; i++) {
@@ -13,15 +13,15 @@ const isVertPathClear = (game, origin, dest, limit = 7) => {
   return true;
 };
 
-const isDiagPathClear = (game, origin, dest, limit = 7) => {
-  console.log('Origin: ', origin, game.board[origin[0]][origin[1]]);
-  console.log('Dest: ', dest, game.board[dest[0]][dest[1]]);
+const isDiagPathClear = (board, origin, dest, limit = 7) => {
+  console.log('Origin: ', origin, board[origin[0]][origin[1]]);
+  console.log('Dest: ', dest, board[dest[0]][dest[1]]);
   // if destination coordinate is located below the original piece
   if (origin[0] < dest[0]) {
     let y = origin[1] - 1;
     for (let i = origin[0] + 1; i < dest[0]; i++) {
-      console.log('Path: ', [i, y], game.board[i][y]);
-      if (game.board[i][y]) {
+      console.log('Path: ', [i, y], board[i][y]);
+      if (board[i][y]) {
         return false;
       }
       y--;
@@ -30,8 +30,8 @@ const isDiagPathClear = (game, origin, dest, limit = 7) => {
     // if destination coordinate is located above the original piece
     let y = origin[1] + 1;
     for (let i = origin[0]; i > dest[0]; i--) {
-      console.log('Path: ', [i, y], game.board[i][y]);
-      if (game.board[i][y]) {
+      console.log('Path: ', [i, y], board[i][y]);
+      if (board[i][y]) {
         return false;
       }
       y++;
@@ -87,7 +87,7 @@ const isLegalMoveKing = (board, origin, dest) => {
 // };
 
 const isLegalMove = (game, origin, dest) => {
-  const pieceType = game.board[origin[0]][origin[1]][1];
+  const pieceType = board[origin[0]][origin[1]][1];
   if (pieceType === 'P') {
     return isLegalMovePawn(game, origin, dest);
   } else if (pieceType === 'R') {
