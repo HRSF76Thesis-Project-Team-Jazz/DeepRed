b4dd7e9d0c6a71e1aeb8ef122f5b6f934e9f0d8f
Ryan Chow <chowryan@gmail.com>
2017-05-24 12:24:13 -0700

    Working on chess logic

---
+++ server/chess/isLegalMove.js
@@ -0,0 +1,81 @@
+// origin/dest are arrays that represent coordinates [M, N]
+
+const isVertPathClear = (game, origin, dest, limit = 7) => {
+  console.log('Origin: ', origin, game.board[origin[0]][origin[1]]);
+  console.log('Dest: ', dest, game.board[dest[0]][dest[1]])
+
+  for (let i = origin[0] + 1; i < dest[0]; i++) {
+    console.log('Path: ', [i, origin[1]], game.board[i][origin[1]]);
+    if (game.board[i][origin[1]]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isLegalMovePawn = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+const isLegalMoveRook = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+const isLegalMoveKnight = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+const isLegalMoveBishop = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+const isLegalMoveQueen = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+const isLegalMoveKing = (board, origin, dest) => {
+  // TODO
+  return true;
+};
+
+// const isKingInCheck = (board) => {
+//   // TODO
+//   return true;
+//
+// };
+//
+// const isCheckMate = (board) => {
+//
+// };
+//
+// const isStalemate = (board) => {
+//   // TODO
+//   return true;
+//
+// };
+
+const isLegalMove = (game, origin, dest) => {
+  const pieceType = game.board[origin[0]][origin[1]][1];
+  if (pieceType === 'P') {
+    return isLegalMovePawn(game, origin, dest);
+  } else if (pieceType === 'R') {
+    return isLegalMoveRook(game, origin, dest);
+  } else if (pieceType === 'N') {
+    return isLegalMoveKnight(game, origin, dest);
+  } else if (pieceType === 'B') {
+    return isLegalMoveBishop(game, origin, dest);
+  } else if (pieceType === 'Q') {
+    return isLegalMoveQueen(game, origin, dest);
+  } else if (pieceType === 'K') {
+    return isLegalMoveKing(game, origin, dest);
+  }
+  return false;
+};
+
+
+exports.module = isLegalMove;
