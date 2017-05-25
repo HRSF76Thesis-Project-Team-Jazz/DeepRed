6c25c663328725801b35c865311f2a48237aa246
Carlo Las Marias <carlol@gmail.com>
2017-05-24 19:07:29 -0700

    remove process env console.logs

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ const app = require('./app');
 
 const db = require('../db');
 
-console.log('** [server/index.js] Environment variables: ', process.env);
-console.log('** [server/index.js] Environment variables -process.env.PORT: ', process.env.PORT);
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
