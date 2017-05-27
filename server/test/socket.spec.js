38ce9d56163e401ce88030006c1732765c888669
Shawn Feng <shawnsfeng@gmail.com>
2017-05-27 11:36:55 -0700

    add socket test

---
+++ server/test/socket.spec.js
@@ -0,0 +1,14 @@
+const should = require('should');
+const io = require('socket.io-client');
+
+const user1 = { 'name': 'Ryan' };
+const user2 = { 'name': 'Carlo' };
+const user3 = { 'name': 'Jason'};
+const user4 = { 'name': 'Shawn'};
+
+describe('socket.io', () => {
+
+  it('should notify user when server side socket connection is on ', () => {
+
+  });
+});
