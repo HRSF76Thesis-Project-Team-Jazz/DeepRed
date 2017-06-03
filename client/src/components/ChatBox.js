58aa856fe87d1dc5cf5d6a4b42143d37edb91b0b
Ryan Chow <chowryan@gmail.com>
2017-06-03 16:27:53 -0700

    wip

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import { red50, red900 } from 'material-ui/styles/colors';
 
 class ChatBox extends Component {
   constructor(props) {
@@ -25,19 +28,27 @@ class ChatBox extends Component {
   render() {
     return (
       <div>
-        <h4>ChatBox</h4>
         <ul id="messages">
           {this.props.messages.map((msg, i) =>
             <h6 key={i + msg}>{msg}</h6>,
           )}
         </ul>
         <form>
-          <input
-            type="text"
+          <TextField
+            hintText="Message Opponent"
+            underlineStyle={{ borderColor: red900 }}
+            underlineFocusStyle={{ borderColor: red900 }}
             value={this.state.message}
             onChange={this.handleChange}
+            style={{ width: 150 }}
           />
-          <button onClick={info => this.submit(info)}>Send</button>
+          <RaisedButton
+            label="Submit"
+            style={{ margin: 12 }}
+            onClick={info => this.submit(info)}
+            backgroundColor="#600003"
+            labelColor={red50}
+          />
         </form>
       </div>
     );
