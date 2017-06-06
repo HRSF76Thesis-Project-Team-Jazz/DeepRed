0891c63b75995f15737d2129a5803cc1ba2e1e9d
jaycoder1 <jason.yu014@gmail.com>
2017-06-06 11:56:23 -0700

    working on DB schema func

--- server/chess/chessSocket.js
+++ server/chess/chessSocket.js
@@ -75,8 +75,10 @@ module.exports = (io, client) => {
   client.on('attemptMove', (origin, dest, selection, clientRoom) => {
     console.log('attempted Move: ', origin, dest);
     console.log('room number: ', clientRoom);
+
     const newState = allGames[clientRoom].movePiece(origin, dest);
     io.in(clientRoom).emit('attemptMoveResult', newState.error, origin, dest, selection, newState.game.turn, newState.castling);
+
   });
 
   client.on('checkLegalMoves', (origin, clientRoom, id) => {
