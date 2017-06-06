bfeb751cb964864299182ecf42c7aff533854b18
Carlo Las Marias <carlol@gmail.com>
2017-06-05 23:31:51 -0700

    castling tests

--- server/test/deepRed.spec.js
+++ server/test/deepRed.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 
 const deepRed = require('../chess/deepRed');
 
-let pieceState = {
+const pieceState = {
   hasMovedWK: false,
   hasMovedWKR: false,
   hasMovedWQR: false,
@@ -237,5 +237,216 @@ describe('End of game checks', () => {
       expect(deepRed.isStalemateBlack(checkmateBoard)).to.eql(false);
       expect(deepRed.isStalemateBlack(stalemateBoard)).to.eql(true);
     });
+  });
+
+  describe('[White] Check Castling', () => {
+    const board = [
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', null, null, 'WR'],
+    ];
+
+    const blockedKRBoard = [
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', 'WB', null, 'WR'],
+    ];
+
+    const blockedQRBoard = [
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, 'WQ', 'WK', null, null, 'WR'],
+    ];
+
+    const checkBoard = [
+      [null, null, null, null, 'BR', null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', null, null, 'WR'],
+    ];
+
+    const attackedCastleBoard = [
+      [null, null, null, 'BR', null, 'BR', null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', null, null, 'WR'],
+    ];
+
+    const attackedKRBoard = [
+      [null, null, null, null, null, 'BR', null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', null, null, 'WR'],
+    ];
+
+    const attackedQRBoard = [
+      [null, null, null, 'BR', null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ['WR', null, null, null, 'WK', null, null, 'WR'],
+    ];
+
+    const kingMovedState = Object.assign({}, pieceState, { hasMovedWK: true });
+    const whiteKRMovedState = Object.assign({}, pieceState, { hasMovedWKR: true });
+    const whiteQRMovedState = Object.assign({}, pieceState, { hasMovedWQR: true });
+
+    it('should know that castling is available', () => {
+      expect(deepRed.getAllMovesWhite(board, pieceState).specialMoves).to.include.members(['O-O', 'O-O-O']);
+    });
+
+    it('should know that castling is disallowed if K or R have moved', () => {
+      expect(deepRed.getAllMovesWhite(board, kingMovedState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesWhite(board, whiteKRMovedState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesWhite(board, whiteQRMovedState).specialMoves).to.not.include('O-O-O');
+    });
+
+    it('should know that castling is not allowed if the path is blocked', () => {
+      expect(deepRed.getAllMovesWhite(blockedKRBoard, pieceState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesWhite(blockedQRBoard, pieceState).specialMoves).to.not.include('O-O-O');
+    });
+
+    it('should know that castling is not allowed if K or the path is attacked', () => {
+      expect(deepRed.getAllMovesWhite(checkBoard, pieceState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesWhite(attackedCastleBoard, pieceState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesWhite(attackedKRBoard, pieceState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesWhite(attackedQRBoard, pieceState).specialMoves).to.not.include('O-O-O');
+    });
+  });
+
+  describe('[Black] Check Castling', () => {
+    const board = [
+      ['BR', null, null, null, 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+    ];
+
+    const blockedKRBoard = [
+      ['BR', null, null, null, 'BK', 'BB', null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+    ];
+
+    const blockedQRBoard = [
+      ['BR', null, null, 'BQ', 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+    ];
+
+    const checkBoard = [
+      ['BR', null, null, null, 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, 'WR', null, null, null],
+    ];
+
+    const attackedCastleBoard = [
+      ['BR', null, null, null, 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, 'WR', null, 'WR', null, null],
+    ];
+
+    const attackedKRBoard = [
+      ['BR', null, null, null, 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, 'WR', null, null],
+    ];
+
+    const attackedQRBoard = [
+      ['BR', null, null, null, 'BK', null, null, 'BR'],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, 'WR', null, null, null, null],
+    ];
+
+    const kingMovedState = Object.assign({}, pieceState, { hasMovedBK: true });
+    const blackKRMovedState = Object.assign({}, pieceState, { hasMovedBKR: true });
+    const blackQRMovedState = Object.assign({}, pieceState, { hasMovedBQR: true });
+
+    it('should know that castling is available', () => {
+      expect(deepRed.getAllMovesBlack(board, pieceState).specialMoves).to.include.members(['O-O', 'O-O-O']);
+    });
+
+    it('should know that castling is disallowed if K or R have moved', () => {
+      expect(deepRed.getAllMovesBlack(board, kingMovedState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesBlack(board, blackKRMovedState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesBlack(board, blackQRMovedState).specialMoves).to.not.include('O-O-O');
+    });
+
+    it('should know that castling is not allowed if the path is blocked', () => {
+      expect(deepRed.getAllMovesBlack(blockedKRBoard, pieceState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesBlack(blockedQRBoard, pieceState).specialMoves).to.not.include('O-O-O');
+    });
+
+    it('should know that castling is not allowed if K or the path is attacked', () => {
+      expect(deepRed.getAllMovesBlack(checkBoard, pieceState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesBlack(attackedCastleBoard, pieceState)).to.not.have.property('specialMoves');
+      expect(deepRed.getAllMovesBlack(attackedKRBoard, pieceState).specialMoves).to.not.include('O-O');
+      expect(deepRed.getAllMovesBlack(attackedQRBoard, pieceState).specialMoves).to.not.include('O-O-O');
+    });
+
   });
 });
