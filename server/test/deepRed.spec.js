2dd41d366596d24eef0be70eabcd7fc71bbd9f4b
Carlo Las Marias <carlol@gmail.com>
2017-06-06 17:34:33 -0700

    black en-passant tests

--- server/test/deepRed.spec.js
+++ server/test/deepRed.spec.js
@@ -10,7 +10,7 @@ const pieceState = {
   hasMovedBKR: false,
   hasMovedBQR: false,
   canEnPassantW: '',
-  canEnPasswantB: '',
+  canEnPassantB: '',
 };
 
 describe('[Deep Red] evaluate available possible moves: ', () => {
@@ -301,6 +301,7 @@ describe('[Black] Check Castling', () => {
 
 });
 
+
 describe('[White] En-Passant', () => {
   const board = [
     ['BR', 'BN', 'BB', 'BK', 'BQ', 'BB', 'BN', 'BR'],
@@ -378,10 +379,94 @@ describe('[White] En-Passant', () => {
   });
 
   it('should know what en-passant moves are available', () => {
-    expect(deepRed.getAllMovesWhite(enPassant0, ep0State).specialMoves).to.deep.include({ move: 'enpassant', from: '31', to: '20', bp: '30' });
-    expect(deepRed.getAllMovesWhite(enPassant1, ep1State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '30', to: '21', bp: '31' }, { move: 'enpassant', from: '32', to: '21', bp: '31' }]);
-    expect(deepRed.getAllMovesWhite(enPassant3, ep3State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '32', to: '23', bp: '33' }, { move: 'enpassant', from: '34', to: '23', bp: '33' }]);
-    expect(deepRed.getAllMovesWhite(enPassant7, ep7State).specialMoves).to.deep.include({ move: 'enpassant', from: '36', to: '27', bp: '37' });
+    expect(deepRed.getAllMovesWhite(enPassant0, ep0State).specialMoves).to.deep.include({ move: 'enpassant', from: '31', to: '20', captured: '30' });
+    expect(deepRed.getAllMovesWhite(enPassant1, ep1State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '30', to: '21', captured: '31' }, { move: 'enpassant', from: '32', to: '21', captured: '31' }]);
+    expect(deepRed.getAllMovesWhite(enPassant3, ep3State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '32', to: '23', captured: '33' }, { move: 'enpassant', from: '34', to: '23', captured: '33' }]);
+    expect(deepRed.getAllMovesWhite(enPassant7, ep7State).specialMoves).to.deep.include({ move: 'enpassant', from: '36', to: '27', captured: '37' });
+  });
+});
+
+describe('[Black] En-Passant', () => {
+  const board = [
+    ['BR', 'BN', 'BB', 'BK', 'BQ', 'BB', 'BN', 'BR'],
+    ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
+    ['WR', 'WN', 'WB', 'WK', 'WQ', 'WB', 'WN', 'WR'],
+  ];
+
+  const enPassant0 = [
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ['WP', 'BP', null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+  ];
+
+  const enPassant1 = [
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ['BP', 'WP', 'BP', null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+  ];
+
+  const enPassant3 = [
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, 'BP', 'WP', 'BP', null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+  ];
+
+  const enPassant7 = [
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, 'BP', 'WP'],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+  ];
+
+  const ep0State = Object.assign({}, pieceState, { canEnPassantB: '40' });
+  const ep1State = Object.assign({}, pieceState, { canEnPassantB: '41' });
+  const ep3State = Object.assign({}, pieceState, { canEnPassantB: '43' });
+  const ep7State = Object.assign({}, pieceState, { canEnPassantB: '47' });
+
+  it('should know that en-passant is not available', () => {
+    expect(deepRed.getAllMovesBlack(board, pieceState)).to.not.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant0, pieceState)).to.not.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant1, pieceState)).to.not.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant3, pieceState)).to.not.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant7, pieceState)).to.not.have.property('specialMoves');
+  });
+
+  it('should know that en-passant is available', () => {
+    expect(deepRed.getAllMovesBlack(enPassant0, ep0State)).to.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant1, ep1State)).to.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant3, ep3State)).to.have.property('specialMoves');
+    expect(deepRed.getAllMovesBlack(enPassant7, ep7State)).to.have.property('specialMoves');
+  });
+
+  it('should know what en-passant moves are available', () => {
+    expect(deepRed.getAllMovesBlack(enPassant0, ep0State).specialMoves).to.deep.include({ move: 'enpassant', from: '41', to: '50', captured: '40' });
+    expect(deepRed.getAllMovesBlack(enPassant1, ep1State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '40', to: '51', captured: '41' }, { move: 'enpassant', from: '42', to: '51', captured: '41' }]);
+    expect(deepRed.getAllMovesBlack(enPassant3, ep3State).specialMoves).to.deep.include.members([{ move: 'enpassant', from: '42', to: '53', captured: '43' }, { move: 'enpassant', from: '44', to: '53', captured: '43' }]);
+    expect(deepRed.getAllMovesBlack(enPassant7, ep7State).specialMoves).to.deep.include({ move: 'enpassant', from: '46', to: '57', captured: '47' });
   });
 });
 
