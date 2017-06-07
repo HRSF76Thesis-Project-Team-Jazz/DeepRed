81acd2529529142d7594367402ae061a7ef36494
Carlo Las Marias <carlol@gmail.com>
2017-06-06 23:53:06 -0700

    clean up ChessGame tests

--- server/chess/ChessGame.js
+++ server/chess/ChessGame.js
@@ -42,23 +42,15 @@ class ChessGame {
     let error = null;
     if (dest === undefined) {
       error = 'Attempted destination is invalid.';
-      console.log(this.board);
-      console.log(error);
       return { game: this, error };
     } else if (!origin || !this.board[origin[0]] || !this.board[origin[0]][origin[1]]) {
       error = 'Origin is invalid.';
-      console.log(this.board);
-      console.log(error);
       return { game: this, error };
     } else if (origin[0] === dest[0] && origin[1] === dest[1]) {
       error = 'Origin and destination cannot be the same.';
-      console.log(this.board);
-      console.log(error);
       return { game: this, error };
     } else if (this.turn !== this.board[origin[0]][origin[1]][0]) {
       error = 'Not your turn.';
-      console.log(this.board);
-      console.log(error);
       return { game: this, error };
     }
     const originPiece = this.board[origin[0]][origin[1]];
@@ -66,8 +58,6 @@ class ChessGame {
     if (destPiece) {
       if (originPiece[0] === destPiece[0]) {
         error = 'Cannot capture your own piece.';
-        console.log(this.board);
-        console.log(error);
         return { game: this, error };
       }
     }
@@ -115,33 +105,15 @@ class ChessGame {
         this.hasMovedBRK = true;
       }
       if (destPiece) {
-        // if (originPiece[0] === destPiece[0]) {
-        //   error = 'Cannot capture your own piece.';
-        //   console.log(this.board);
-        //   console.log(error);
-        //   return { game: this, error };
-        // }
-        // this.history += moveToPGNString(this.board, origin, dest, this.count);
         this.capturePiece(destPiece);
       }
-      // this.history[this.turn] = this.history[this.turn] || [];
-      // this.history[this.turn].push(origin);
-      // this.history[this.turn].push(dest);
-      // if (originPiece[0] === 'B') {
-      //   this.turn += 1;
-      // }
       this.turn = (this.turn === 'W') ? 'B' : 'W';
       this.board[dest[0]][dest[1]] = originPiece;
       this.board[origin[0]][origin[1]] = null;
       // check for check/checkmate/stalemate
-      // console.log('--------------', this.history);
-      console.log('Move piece is successful');
-      console.log(this.board);
       return { game: this, error, castling: legalMoveResult.castling };
     }
     error = 'Move is not allowed.';
-    console.log(this.board);
-    console.log(error);
     return { game: this, error };
   }
 
@@ -165,77 +137,6 @@ class ChessGame {
     }
     return resultBoard;
   }
-
-  // pawnPromotion(piece, origin, dest) {
-  //   let originPiece = this.board[origin[0]][origin[1]];
-  //   let destPiece = this.board[dest[0]][dest[1]];
-  //
-  //   if (isLegalMove(this.board, origin, dest)) {
-  //     if (piece[0] === 'W' && origin[0] === 1) {
-  //       if (dest[0] === 0) {
-  //         destPiece = 'WQ';
-  //         originPiece = null;
-  //       }
-  //     }
-  //     if (piece[0] === 'B' && origin[0] === 6) {
-  //       if (dest[0] === 7) {
-  //         destPiece = 'BQ';
-  //         originPiece = null;
-  //       }
-  //     }
-  //     this.history[this.turn] = this.history[this.turn] || [];
-  //     this.history[this.turn].push(origin);
-  //     this.history[this.turn].push(dest);
-  //     if (originPiece[0] === 'B') {
-  //       this.turn += 1;
-  //     }
-  //     this.count += 1;
-  //   }
-  // }
-  //
-  // castling(piece, origin, dest) {
-  //   let originPiece = this.board[origin[0]][origin[1]];
-  //   let destPiece = this.board[dest[0]][dest[1]];
-  //
-  //   // if king and rook hasnt moved
-  //   // king is not in Check
-  //
-  //   if (isHorizPathClear(this.board, origin, dest, limit = 7)) {
-  //     if (piece[0] === 'W') {
-  //       if (dest[0] == 7 && dest[1] == 0) {
-  //         this.board[7][2] = 'WK';
-  //         this.board[7][3] = 'WR';
-  //         originPiece = null;
-  //         destPiece = null;
-  //       } else if (dest[0] === 7 && dest[1] === 7) {
-  //         this.board[7][6] = 'WK';
-  //         this.board[7][5] = 'WR';
-  //         originPiece = null;
-  //         destPiece = null;
-  //       }
-  //     } else if (piece[0] === 'B') {
-  //       if (dest[0] === 0 && dest[1] === 0) {
-  //         this.board[0][2] = 'BK';
-  //         this.board[0][3] = 'BR';
-  //         originPiece = null;
-  //         destPiece = null;
-  //       } else if (dest[0] === 0 && dest[1] === 7) {
-  //         this.board[0][6] = 'BK';
-  //         this.board[0][5] = 'BR';
-  //         originPiece = null;
-  //         destPiece = null;
-  //       }
-  //     }
-  //     this.history[this.turn] = this.history[this.turn] || [];
-  //     this.history[this.turn].push(origin);
-  //     this.history[this.turn].push(dest);
-  //     if (originPiece[0] === 'B') {
-  //       this.turn += 1;
-  //     }
-  //     this.count += 1;
-  //   }
-  // }
-  //
 
 }
 
