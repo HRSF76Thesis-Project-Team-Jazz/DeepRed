34e16c76117781a5cb6e50910e27cc4e0494eb00
jaycoder1 <jason.yu014@gmail.com>
2017-06-09 17:28:35 -0700

    progress on 2nd chatbox

---
+++ client/src/components/ChatBox2.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import { red50, red900 } from 'material-ui/styles/colors';
+// import Messages from './Messages'
+
+import './css/ChatBox.css';
+
+class ChatBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  submit(info) {
+    info.preventDefault();
+    this.props.sendMessage(this.state.message);
+    this.state.message = '';
+  }
+
+  handleChange(info) {
+    this.setState({
+      message: info.target.value,
+    });
+  }
+
+  handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      this.submit(event);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <TextField
+            className="text-field"
+            style={{ width: '18vw' }}
+            hintText="Message Opponent"
+            underlineStyle={{ borderColor: red900 }}
+            underlineFocusStyle={{ borderColor: red900 }}
+            value={this.state.message}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          {/* <RaisedButton
+            label="Submit"
+            style={{ margin: 12 }}
+            onClick={info => this.submit(info)}
+            backgroundColor="#600003"
+            labelColor={red50}
+          /> */}
+        </form>
+        </div>
+    );
+  }
+}
+
+
+export default ChatBox;
