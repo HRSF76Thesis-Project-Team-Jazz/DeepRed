363e3e9f9f7454c2aefd19c1ebb1df2e8f349679
Carlo Las Marias <carlol@gmail.com>
2017-06-15 14:19:33 -0700

    router update

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -4,7 +4,6 @@ const express = require('express');
 
 const router = express.Router();
 const ProfileController = require('../controllers').Profiles;
-const middleware = require('../middleware');
 
 router.route('/')
   .get(ProfileController.getAll)
@@ -22,10 +21,5 @@ router.route('/:id')
   .put(ProfileController.update)
   // .delete(ProfileController.deleteOne)
   ;
-
-router.route('/*')
-  .get(middleware.auth.verify, (req, res) => {
-    res.render('index.ejs');
-  });
 
 module.exports = router;
